2d01560f8ad153a96708e336dce2d0250cf33241
jaarguello <johnny.arguello007@gmail.com>
2022-01-31 14:43:59 -0300

    add enviroment variables

--- src/utils/firebase.jsx
+++ src/utils/firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6GIdudDSW0ukYrnnZKdkXU3b9vteESvc",
-  authDomain: "survey-typeform.firebaseapp.com",
-  projectId: "survey-typeform",
-  storageBucket: "survey-typeform.appspot.com",
-  messagingSenderId: "480403954572",
-  appId: "1:480403954572:web:40f2674e4f75047ab614a9",
-  measurementId: "G-DNGZNDHG1H",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 const app = initializeApp(firebaseConfig);
 
